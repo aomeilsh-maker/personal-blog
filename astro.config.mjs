f81108b493b4a495051f2535c8f4b69e81a1f5cc
aomeilsh <aomeilsh@aomeilshdeMac-mini.local>
2026-02-24 20:28:46 +0900

    Fix duplicated nested personal-blog assets path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.SITE_URL || 'https://example.com',
-	base: process.env.BASE_PATH || '/personal-blog',
+	base: process.env.BASE_PATH || '/',
 	integrations: [mdx(), sitemap()],
 });
